8b51f93b2c83ffac26796c2858cf029e9cda5833
lai2-24 <lai2-24@itismeucci.com>
2024-12-17 14:38:25 +0100

    non funziona

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -12,75 +12,88 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
-    ServerSocket ss = new ServerSocket(8080);
 
-    while(true){
-        Socket s = ss.accept();
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        
-        String firsLine = in.readLine();
-        System.out.println(firsLine);
-        String [] request = firsLine.split(" ");
+        ServerSocket ss = new ServerSocket(8080);
 
-        String method = request[0];
-        String resource = request[1];
-        String version = request[2];
-        
+        while (true) {
+            Socket s = ss.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String header;
-        do {
-            header = in.readLine();
-            System.out.println(header);
-        }while(!header.isEmpty());
+            String firsLine = in.readLine();
+            System.out.println(firsLine);
+            String[] request = firsLine.split(" ");
 
-        System.out.println("richiesta terminata");
+            String method = request[0];
+            String resource = request[1]; // quella da fare gli if
+            String version = request[2];
 
-        File file = new File("htdcocs" + resource );
-       
+            String header;
+            do {
+                header = in.readLine();
+                System.out.println(header);
+            } while (!header.isEmpty());
+            System.out.println("richiesta terminata");
 
-        //risposta
-        if (file.exists()){
 
-            out.writeBytes("HTTP/1.1 200 ok\n");
-            out.writeBytes("Content-Type:" + getContentType(file) + "\n");
-            out.writeBytes("Content-Lenght: " + file.length() + " \n");
-            out.writeBytes("\n"); 
-            InputStream input = new FileInputStream(file);
-            byte[] buf = new byte[8192];
-            int n;
-            while ((n = input.read(buf)) != -1 ){
-                out.write(buf, 0, n);
+             if (resource.endsWith("/")) {
+                resource = resource + "/index.html";
             }
-            input.close();
-            
-        }else{
-            out.writeBytes("HTTP/1.1 404 Not Found\n");
-            out.writeBytes("Content-Lenght: 0\n");
-            out.writeBytes("\n");
-        }    
 
-        s.close();
+            File file = new File("htdocs" + resource);
 
-    }
+            if (file.isDirectory()) {
+                out.writeBytes("HTTP/1.1 301 Moved Permanently\n");
+                out.writeBytes("Content-Lenght: 0\n");
+                out.writeBytes("Location: " + resource + "/\n");
+                out.writeBytes("\n");
+
+            }else if (file.exists()) {
+                out.writeBytes("HTTP/1.1 200 OK\n");
+                out.writeBytes("Content-Type: " + getContentType(file) + "\n");
+                out.writeBytes("Content-Length: " + file.length() + " \n");
+                out.writeBytes("\n");
+                
+                InputStream input = new FileInputStream(file);
+                byte[] buf = new byte[8192];
+                int n; 
+                while ((n = input.read(buf)) != -1) {
+                    out.write(buf, 0, n);
+                }
+                input.close();
+
+
+
+            } else {
+
+                //String responseBody = "pagina non trovata o non disponibile";
+                out.writeBytes("HTTP/1.1 404 Not found\n");
+                out.writeBytes("Content-Lenght: 0 \n");
+                out.writeBytes("\n");
+            }
+
+            s.close();
+
+        }
+
     }
 
     private static String getContentType(File f){
-        String [] s = f.getName().split("\\ .");
-        String ext = s[s.length -1 ];
+        String[] s = f.getName().split("\\.");
+        String ext = s[s.length - 1];
         switch (ext) {
             case "html":
             case "htm":
                 return "text/html";
+            
             case "png":
                 return "image/png";
+
             case "css":
-                return "text.css";
+                return "text/css";
+
             default:
                 return "";
         }
     }
-
-
 }
